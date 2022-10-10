2deb21cc8b5b22173ab17b333ec356a53d24e0c1
kwonminki <olleh401@naver.com>
2022-10-10 22:38:27 +0900

    8th commit

--- resrc/codec.py
+++ resrc/codec.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import shutil
 
 
 def listdirs(rootdir, _files=[]):
@@ -11,9 +12,37 @@ def listdirs(rootdir, _files=[]):
 
 # Get all files in the directory
 files = []
-listdirs("video_quality", files)
+listdirs("video_ddim", files)
 
 
 for file_name in files:
-    os.system(f"ffmpeg -i {file_name} -vcodec libx264 {file_name} -y")
+    vidcap = cv2.VideoCapture(f'{file_name}')
+    success,image = vidcap.read()
+    count = 0
+    dir_name = file_name.split(".")[0]
+    os.makedirs(dir_name, exist_ok=True)
+    while success:    
+        cv2.imwrite(f"{dir_name}/{count}.png", image)     # save frame as JPEG file
+        success,image = vidcap.read()
+        # print('Read a new frame: ', success)
+        count += 1
+
+print("finish! convert video to frame")
+
+
+for filename in files:
+    img_array = []
+    if filename.endswith(".mp4"):
+        # get file directory
+        dir = os.path.dirname(filename)
+        # get file name
+        name = os.path.basename(filename)
+        # get file name without extension
+        name = os.path.splitext(name)[0]
+        # get directory which is the name of the video
+        image_dir = os.path.join(dir, name)
+
+        os.system(f"ffmpeg -f image2 -framerate 10 -i {image_dir}/%d.png -c:v libx264 -preset veryslow {filename} -y")
+
+        shutil.rmtree(image_dir)
 
